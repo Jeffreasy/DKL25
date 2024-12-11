778553a1011a8c509e1a55a9147b0a1d6acf3327
Jeffrey <laventejeffrey@gmail.com>
2024-12-11 22:14:54 +0100

    Add core components, Home page and update dependencies

---
+++ src/pages/Home.tsx
@@ -0,0 +1,15 @@
+import { 
+  PartnerCarrousel,
+  HeroSection,
+  TitleSection,
+  CTACards,
+  PhotoGallery,
+  VideoGallery,
+  DKLSocials,
+  DKLSponsors 
+} from '@/components';
+
+import { 
+  InschrijfModal, 
+  DonatieModal 
+} from '@/components/modals'; 
