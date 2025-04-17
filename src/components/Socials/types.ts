3dd745fe1f1d2955813ddeae5884083764b78334
Jeffreasy <158185183+Jeffreasy@users.noreply.github.com>
2025-04-17 23:55:26 +0200

    fix: Adjust SocialLink type for nullable color classes

--- src/components/Socials/types.ts
+++ src/components/Socials/types.ts
@@ -3,6 +3,6 @@ export type SocialPlatform = 'facebook' | 'instagram' | 'youtube' | 'linkedin';
 export interface SocialLink {
   platform: SocialPlatform;
   url: string;
-  bgColorClass: string;
-  iconColorClass: string;
+  bgColorClass: string | null;
+  iconColorClass: string | null;
 } 
